a5bc8302badac6deaf0a6cd9e8731485e13f6bb9
fredie <fredie.unabia@infectiousmedia.com>
2019-02-14 15:13:23 +0000

    Updates

---
+++ precache-manifest.7e336b0c8c29d386c494f467a8e657e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/floof/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "511a5e44bd496327f7f3",
+    "url": "/floof/static/js/runtime~main.511a5e44.js"
+  },
+  {
+    "revision": "f188a594233a3d9e1d91",
+    "url": "/floof/static/js/main.f188a594.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/floof/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "f188a594233a3d9e1d91",
+    "url": "/floof/static/css/main.3ab5aa9d.chunk.css"
+  },
+  {
+    "revision": "7dc370bd641e9417cc04b9bf764c0c82",
+    "url": "/floof/index.html"
+  }
+];
